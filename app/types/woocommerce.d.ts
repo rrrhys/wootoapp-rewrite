5817ae8ca4544712e6f2626d4da0331115b1ef8b
Rhys <rrrhys@gmail.com>
2019-08-26 18:26:02 +1000

    Add products ts.

--- app/types/woocommerce.d.ts
+++ app/types/woocommerce.d.ts
@@ -11,6 +11,16 @@ export interface Category {
   _links: Links;
 }
 
+export interface Collection {
+  href: string;
+}
+
+export interface Dimensions {
+  length: string;
+  width: string;
+  height: string;
+}
+
 export interface Image {
   id: number;
   date_created: Date;
@@ -18,19 +28,91 @@ export interface Image {
   date_modified: Date;
   date_modified_gmt: Date;
   src: string;
-  title: string;
+  name: string;
   alt: string;
-}
-
-export interface Self {
-  href: string;
-}
-
-export interface Collection {
-  href: string;
+  position: number;
 }
 
 export interface Links {
   self: Self[];
   collection: Collection[];
+}
+
+export interface MetaDatum {
+  id: number;
+  key: string;
+  value: string;
+}
+
+export interface Product {
+  id: number;
+  name: string;
+  slug: string;
+  permalink: string;
+  date_created: Date;
+  date_created_gmt: Date;
+  date_modified: Date;
+  date_modified_gmt: Date;
+  type: string;
+  status: string;
+  featured: boolean;
+  catalog_visibility: string;
+  description: string;
+  short_description: string;
+  sku: string;
+  price: string;
+  regular_price: string;
+  sale_price: string;
+  date_on_sale_from: null;
+  date_on_sale_from_gmt: null;
+  date_on_sale_to: null;
+  date_on_sale_to_gmt: null;
+  price_html: string;
+  on_sale: boolean;
+  purchasable: boolean;
+  total_sales: number;
+  virtual: boolean;
+  downloadable: boolean;
+  downloads: any[];
+  download_limit: number;
+  download_expiry: number;
+  external_url: string;
+  button_text: string;
+  tax_status: string;
+  tax_class: string;
+  manage_stock: boolean;
+  stock_quantity: null;
+  in_stock: boolean;
+  backorders: string;
+  backorders_allowed: boolean;
+  backordered: boolean;
+  sold_individually: boolean;
+  weight: string;
+  dimensions: Dimensions;
+  shipping_required: boolean;
+  shipping_taxable: boolean;
+  shipping_class: string;
+  shipping_class_id: number;
+  reviews_allowed: boolean;
+  average_rating: string;
+  rating_count: number;
+  related_ids: any[];
+  upsell_ids: any[];
+  cross_sell_ids: any[];
+  parent_id: number;
+  purchase_note: string;
+  categories: Category[];
+  tags: Category[];
+  images: Image[];
+  attributes: any[];
+  default_attributes: any[];
+  variations: any[];
+  grouped_products: any[];
+  menu_order: number;
+  meta_data: MetaDatum[];
+  _links: Links;
+}
+
+export interface Self {
+  href: string;
 }
