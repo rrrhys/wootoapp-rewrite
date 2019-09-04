8d9e53363581a913ddadea829985e652f50c631e
Rhys <rrrhys@gmail.com>
2019-09-04 19:35:17 +1000

    add fetch individual product & variations of a product to redux.

--- app/actions/products.ts
+++ app/actions/products.ts
@@ -1,32 +1,69 @@
 const PRODUCTS_IN_CATEGORY_LOADED = "PRODUCTS_IN_CATEGORY_LOADED";
+const VARIATION_BY_PRODUCT_ID_LOADED = "VARIATION_BY_PRODUCT_ID_LOADED";
+const PRODUCT_LOADED = "PRODUCT_LOADED";
+
+import { Product, Variation } from "../types/woocommerce";
 
 import api from "./../../data/api";
 
 const loadProductsInCategory = (category: number, page: number = 1) => {
-	return (dispatch, getState) => {
-		api.products.list({ category, page: page }).then(r => {
-			dispatch(getProductsInCategorySuccess(category, r, page));
-		});
-		// TODO ERROR HANDLE.
-	};
+  return (dispatch, getState) => {
+    api.products.list({ category, page: page }).then((r: Array<Product>) => {
+      r.forEach((p: Product) => {
+        if (p.variations.length > 0) {
+          // fetch child variable types.
+          dispatch(loadVariationByProductId(p.id));
+        }
+      });
+      dispatch(getProductsInCategorySuccess(category, r, page));
+    });
+    // TODO ERROR HANDLE.
+  };
 };
 
+const loadVariationByProductId = (id: number) => {
+  return (dispatch, getState) => {
+    api.products.variations.get(id).then((v: Array<Variation>) => {
+      dispatch(getVariationByProductIdSuccess(id, v));
+    });
+  };
+};
 const loadProductById = (id: number) => {
-	// TODO: Load this product fresh from server, and update it in the products store.
-	return (dispatch, getState) => {};
+  // TODO: Load this product fresh from server, and update it in the products store.
+  return (dispatch, getState) => {
+    api.products
+      .get(id)
+      .then((product: Product) => dispatch(getProductSuccess(id, product)));
+  };
+};
+
+const getProductSuccess = (id, product) => {
+  return {
+    type: PRODUCT_LOADED,
+    id,
+    product
+  };
+};
+const getVariationByProductIdSuccess = (product_id, variations) => {
+  return {
+    type: VARIATION_BY_PRODUCT_ID_LOADED,
+    product_id,
+    variations
+  };
 };
 
 const getProductsInCategorySuccess = (category, results, page) => {
-	return {
-		type: PRODUCTS_IN_CATEGORY_LOADED,
-		category,
-		results,
-		page,
-	};
+  return {
+    type: PRODUCTS_IN_CATEGORY_LOADED,
+    category,
+    results,
+    page
+  };
 };
 
 export default {
-	PRODUCTS_IN_CATEGORY_LOADED,
-	loadProductsInCategory,
-	loadProductById,
+  PRODUCTS_IN_CATEGORY_LOADED,
+  VARIATION_BY_PRODUCT_ID_LOADED,
+  loadProductsInCategory,
+  loadProductById
 };
--- app/reducers/products.ts
+++ app/reducers/products.ts
@@ -1,66 +1,93 @@
+import { Product, Variation } from "../types/woocommerce";
+
 import Actions from "../actions";
-import { Product } from "../types/woocommerce";
 import _ from "lodash";
 
 export interface IProducts {
-	byId: Array<number>;
-	byPage: { [id: number]: Array<number> };
-	byCategoryId: { [id: number]: Array<number> };
-	products: { [id: number]: Product };
-	lastUpdated: boolean | Date;
+  byId: Array<number>;
+  byPage: { [id: number]: Array<number> };
+  byCategoryId: { [id: number]: Array<number> };
+  variations: { [product_id: number]: Array<Variation> };
+  products: { [id: number]: Product };
+  lastUpdated: boolean | Date;
 }
 
-const { PRODUCTS_IN_CATEGORY_LOADED } = Actions;
+const {
+  PRODUCTS_IN_CATEGORY_LOADED,
+  VARIATION_BY_PRODUCT_ID_LOADED,
+  PRODUCT_LOADED
+} = Actions;
 
 const initialState: IProducts = {
-	byId: [],
-	byPage: {},
-	products: {},
-	byCategoryId: {},
-	lastUpdated: false,
+  byId: [],
+  byPage: {},
+  products: {},
+  variations: {},
+  byCategoryId: {},
+  lastUpdated: false
 };
 
 const products = (
-	state = initialState,
-	action: {
-		results: Array<Product>;
-		category: number;
-		page: number;
-		type: string;
-	}
+  state = initialState,
+  action: {
+    results: Array<Product>;
+    category: number;
+    page: number;
+    type: string;
+  }
 ) => {
-	// type category results page
-	switch (action.type) {
-		case PRODUCTS_IN_CATEGORY_LOADED:
-			const newState = _.cloneDeep(state);
-			const { results, category, page } = action;
-			if (results) {
-				_.forEach(results, p => {
-					newState.products[p.id] = p;
-				});
-				const ids = results.map(r => r.id);
-				newState.byId = _.uniq([...ids, ...newState.byId]);
+  // type category results page
+  switch (action.type) {
+    case VARIATION_BY_PRODUCT_ID_LOADED:
+      let variations = _.cloneDeep(state.variations);
+      const { product_id, variations: thisVariations } = action;
+      if (product_id) {
+        variations[product_id] = thisVariations;
 
-				// store ids for ref by page.
-				newState.byPage = _.get(newState, "byPage", []);
-				newState.byPage[page] = ids;
+        return { ...state, variations };
+      }
+      break;
+    case PRODUCT_LOADED:
+      let products = _.cloneDeep(state.products);
+      const { id, product } = action;
+      products[id] = product;
 
-				// initialise if doesnt exist.
+      return { ...state, products };
+      break;
+    case PRODUCTS_IN_CATEGORY_LOADED:
+      const newState = _.cloneDeep(state);
+      const { results, category, page } = action;
+      if (results) {
+        _.forEach(results, p => {
+          newState.products[p.id] = p;
+        });
+        const ids = results.map(r => r.id);
+        newState.byId = _.uniq([...ids, ...newState.byId]);
 
-				newState.byCategoryId[category] = _.get(newState.byCategoryId, category, []);
+        // store ids for ref by page.
+        newState.byPage = _.get(newState, "byPage", []);
+        newState.byPage[page] = ids;
 
-				// store ids for ref by category.
-				newState.byCategoryId[category] = ids;
+        // initialise if doesnt exist.
 
-				return {
-					...state,
-					...newState,
-					lastUpdated: new Date(),
-				};
-			}
-			break;
-	}
-	return state;
+        newState.byCategoryId[category] = _.get(
+          newState.byCategoryId,
+          category,
+          []
+        );
+
+        // store ids for ref by category.
+        newState.byCategoryId[category] = ids;
+
+        return {
+          ...state,
+          ...newState,
+          lastUpdated: new Date()
+        };
+      }
+      break;
+  }
+  return state;
 };
 
 export default products;
