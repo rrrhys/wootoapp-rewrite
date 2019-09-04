b7e780cc9bf1deb911ea74590d3cf6c479bcffa9
Rhys <rrrhys@gmail.com>
2019-09-04 19:35:37 +1000

    Add attribute pickers to product screen.

--- app/screens/ProductScreen.tsx
+++ app/screens/ProductScreen.tsx
@@ -1,5 +1,5 @@
+import { Attribute, Product, Variation } from "../types/woocommerce";
 import { Button, Icon, Image, Text } from "react-native-elements";
-import Carousel, { Pagination } from "react-native-snap-carousel";
 import { Dimensions, SafeAreaView, ScrollView, View } from "react-native";
 import ScrollableTabView, {
   ScrollableTabBar
@@ -7,13 +7,16 @@ import ScrollableTabView, {
 import { rules, styles } from "../styles";
 
 import Actions from "../actions";
+import AddToCartButton from "../components/AddtoCartButton";
 import BackButtonOverlay from "../components/BackButtonOverlay";
+import ImageCarousel from "../components/ImageCarousel";
+import KeyValuePicker from "../components/KeyValuePicker";
 import Loading from "../components/Loading";
-import { Product } from "../types/woocommerce";
 import ProductDescription from "../components/ProductDescription";
 import React from "react";
 import { connect } from "react-redux";
 
+export const INTERNAL_QUANTITY = "INTERNAL_QUANTITY";
 export interface IProductScreenProps {
   navigation: {
     state: {
@@ -25,17 +28,25 @@ export interface IProductScreenProps {
   };
   loadProductById: (id: number) => void;
   product: Product;
+  variations: Array<Variation>;
 }
 
 export interface IProductScreenState {
   activeSlide: number;
+  quantity: number;
+  attributesSelected: { [id: string]: string };
 }
 
 class ProductScreen extends React.Component<
   IProductScreenProps,
   IProductScreenState
 > {
-  _carousel;
+  state = {
+    activeSlide: 0,
+    quantity: 1,
+    attributesSelected: {}
+  };
+
   static navigationOptions = ({ navigation, navigationOptions }) => {
     return {
       hideHeader: true
@@ -45,35 +56,30 @@ class ProductScreen extends React.Component<
   constructor(props: IProductScreenProps) {
     super(props);
     props.loadProductById(props.navigation.state.params.product.id);
-
-    this.state = {
-      activeSlide: 0
-    };
   }
 
   getHeight = () => {
     return Dimensions.get("window").height - 44;
   };
 
-  _renderItem({ item, index }) {
-    const { width } = Dimensions.get("window");
-    let imageStyle = {
-      width: width,
-      height: width,
-      margin: 0
-    };
-
-    return (
-      <View>
-        <Image source={{ uri: item.src }} style={imageStyle} />
-      </View>
-    );
-  }
+  valueChanged = (key: string, value: number) => {
+    switch (key) {
+      case INTERNAL_QUANTITY:
+        this.setState({
+          quantity: value
+        });
+        break;
+    }
+  };
 
   render() {
-    const { product } = this.props;
+    const { product, variations } = this.props;
 
+    const { quantity } = this.state;
     const { description } = product;
+
+    console.log(product);
+    console.log(variations);
 
     const { width, height } = Dimensions.get("window");
     return product ? (
@@ -85,43 +91,13 @@ class ProductScreen extends React.Component<
         }}
       >
         <ScrollView>
-          <View style={{ maxHeight: width }}>
-            <Carousel
-              ref={c => {
-                this._carousel = c;
-              }}
-              data={product.images}
-              renderItem={this._renderItem}
-              sliderWidth={width}
-              itemWidth={width}
-              itemHeight={width}
-              sliderHeight={width}
-              onSnapToItem={index => this.setState({ activeSlide: index })}
-            />
-          </View>
-
-          {product && product.images.length > 1 && (
-            <View style={{ marginTop: -50 }}>
-              <Pagination
-                dotsLength={product.images.length}
-                activeDotIndex={this.state.activeSlide}
-                dotStyle={{
-                  width: 8,
-                  height: 8,
-                  borderRadius: 5,
-                  marginHorizontal: 4,
-                  backgroundColor: "#000000"
-                }}
-                inactiveDotStyle={
-                  {
-                    // Define styles for inactive dots here
-                  }
-                }
-                inactiveDotOpacity={0.2}
-                inactiveDotScale={0.6}
-              />
-            </View>
-          )}
+          <ImageCarousel
+            height={width}
+            width={width}
+            onSnapToItem={index => this.setState({ activeSlide: index })}
+            activeSlide={this.state.activeSlide}
+            images={product && product.images}
+          />
 
           <View>
             <Text h2 style={styles.productHeading}>
@@ -156,10 +132,40 @@ class ProductScreen extends React.Component<
 
         <BackButtonOverlay onPress={() => this.props.navigation.goBack()} />
 
+        <KeyValuePicker
+          label="quantity"
+          key={INTERNAL_QUANTITY}
+          values={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+          currentValue={quantity}
+          onValueChanged={this.valueChanged}
+        />
+
+        {product.attributes.map((a: Attribute) => {
+          /* attributes: Array(1)
+0:
+id: 0
+name: "Colours"
+options: (3) ["Khaki", "Green", "Charcoal"]
+position: 0
+variation: true
+visible: true
+__proto__: Object
+length: 1
+*/
+          return (
+            a.visible && (
+              <KeyValuePicker
+                key={a.id}
+                label={a.name}
+                values={a.options}
+                currentValue={this.state.attributesSelected[a.id]}
+                defaultValue={a.options[a.position]}
+              />
+            )
+          );
+        })}
         <SafeAreaView>
-          <View style={{ padding: rules.padding }}>
-            <Button title="Add to Cart" />
-          </View>
+          <AddToCartButton product={product} variations={variations} />
         </SafeAreaView>
       </View>
     ) : (
@@ -175,7 +181,8 @@ const select = (store, ownProps: IProductScreenProps) => {
     cart: store.cart,
     ui: store.ui,
     categories: store.categories,
-    product: store.products.products[product.id]
+    product: store.products.products[product.id],
+    variations: store.products.variations[product.id]
   };
 };
 
