8c2116efbbf9e68e2a84f820efbabcf292242fcf
rhys <rrrhys@gmail.com>
2019-10-13 22:43:09 +1100

    uses new serverless back-end.

--- app/actions/shop.ts
+++ app/actions/shop.ts
@@ -2,19 +2,19 @@ import api from "./../../data/api";
 
 const STORE_LOADED = "STORE_LOADED";
 const loadShop = () => {
-  return (dispatch, getState) => {
-    api.store().then(r => {
-      dispatch(storeLoadedSuccess(r));
-    });
-    // TODO ERROR HANDLE.
-  };
+	return (dispatch, getState) => {
+		api.store().then(r => {
+			dispatch(storeLoadedSuccess(r));
+		});
+		// TODO ERROR HANDLE.
+	};
 };
 
 const storeLoadedSuccess = result => {
-  return {
-    type: STORE_LOADED,
-    result
-  };
+	return {
+		type: STORE_LOADED,
+		result,
+	};
 };
 
 export default { loadShop, STORE_LOADED };
--- env.ts
+++ env.ts
@@ -5,25 +5,21 @@ let querystringConfig: boolean | any = false;
 
 const parsed = queryString.parse(location.search);
 if (parsed.config) {
-  querystringConfig = Base64.decode(parsed.config);
-  querystringConfig = JSON.parse(querystringConfig);
+	querystringConfig = Base64.decode(parsed.config);
+	querystringConfig = JSON.parse(querystringConfig);
 }
 
 const config = querystringConfig
-  ? querystringConfig
-  : {
-      version: 0,
-      endpoint: "https://edge-test.wootoapp.com",
-      name: "CartCatch LS",
-      store_id: 4,
-      scheme: "https://",
-      platform: "WooCommerce",
-      plugin_endpoint:
-        "/wp-admin/admin-ajax.php?action=wootoapp_execute&method={method}",
-      publishable_key: "S1Iey3VF7r",
-      facebook_app_id: "1600978189964022",
-      access_jwt:
-        "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJjbGllbnQiOiJNb2JpbGUgQXBwbGljYXRpb24iLCJyZXF1ZXN0IjoiUmVhZCBTdG9yZSIsInN0b3JlIjo0LCJpYXQiOjE1NjI0OTIzOTJ9.L3eKwpDpFe0jcGu80PX6r7YREr4yw-aTQbgyWA8Rt_8"
-    };
+	? querystringConfig
+	: {
+			version: 0,
+			endpoint: "livestore.cartcatch.com",
+			name: "CartCatch LS",
+			platform: "WooCommerce",
+			plugin_endpoint: "/wp-admin/admin-ajax.php?action=wootoapp_execute&method={method}",
+			facebook_app_id: "1600978189964022",
+			access_jwt:
+				"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnQiOiJNb2JpbGUgQXBwbGljYXRpb24iLCJyZXF1ZXN0IjoiUmVhZCBTdG9yZSIsInN0b3JlIjoibGl2ZXN0b3JlLmNhcnRjYXRjaC5jb20iLCJpYXQiOjE1NjI0OTIzOTIsImVudmlyb25tZW50IjoiZGV2In0.TkZB7taAEvw2H_iAR2tiL-cesXSg6sotPnSEe94zS-k",
+	  };
 
 export default config;
